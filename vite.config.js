43b4f949751dd96920c0fa7db8dbf53798364fd8
devCalf <caique.rosa@epiousion.com.br>
2024-10-14 00:08:55 -0300

    base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: 'https://Calf97.github.io/web-cursos/'
+  base: 'https://Calf97.github.io/wb-cursos/'
 })
